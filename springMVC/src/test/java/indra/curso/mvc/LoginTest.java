72a3c91b9ca029f0b2581b7d0e11a026a85b3b9c
formacion <formacion@ANSFORM29.indra.es>
2019-02-20 17:43:10 +0100

    prueba

---
+++ springMVC/src/test/java/indra/curso/mvc/LoginTest.java
@@ -0,0 +1,14 @@
+package indra.curso.mvc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class LoginTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
