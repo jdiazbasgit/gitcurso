5681ab95a98d71aa80a6ba2b7e093ef3405f0deb
formacion <formacion@ANSFORM36.indra.es>
2019-02-20 17:07:45 +0100

    he subido la class controllerTest

--- springMVC/src/test/java/indra/curso/mvc/HomeControllerTest.java
+++ springMVC/src/test/java/indra/curso/mvc/HomeControllerTest.java
@@ -2,6 +2,8 @@ package indra.curso.mvc;
 
 import static org.junit.Assert.assertNotNull;
 
+import javax.inject.Inject;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +13,9 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.support.DependencyInjectionTestExecutionListener;
 import org.springframework.test.context.support.DirtiesContextTestExecutionListener;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.servlet.ModelAndView;
 
+import indra.curso.mvc.beans.Login;
 import indra.curso.mvc.configuracion.Configuracion;
 
 
@@ -26,13 +30,62 @@ public class HomeControllerTest {
 
 	@Autowired
 	private HomeController homeController;
-	
-	
+	private Login login;
 	
 	@Test
 	public void test() {
 		
-		assertNotNull(homeController.home());
+		
+		assertController(homeController.login(login));
+		
 	}
+
+
+	private void assertController(ModelAndView login) {
+		// TODO Auto-generated method stub
+		
+	}
+
+
+	/**
+	 * @return the homeController
+	 */
+	public HomeController getHomeController() {
+		return homeController;
+	}
+
+
+	/**
+	 * @param homeController the homeController to set
+	 */
+	public void setHomeController(HomeController homeController) {
+		this.homeController = homeController;
+	}
+
+
+	/**
+	 * @return the login
+	 */
+	public Login getLogin() {
+		return login;
+	}
+
+
+	/**
+	 * @param login the login to set
+	 */
+	public void setLogin(Login login) {
+		this.login = login;
+	}
+	
+	
+	
+	
+
+
+
+	
+	
+
 
 }
