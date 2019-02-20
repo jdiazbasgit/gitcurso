03e5eff6b4d39306b4ae2ff370794e2f3b7042c1
formacion <formacion@ANSFORM41.indra.es>
2019-02-20 17:43:34 +0100

    probando la linea de comandos XD XD XD

---
+++ springMVC/src/test/java/indra/curso/mvc/LoginTest.java
@@ -0,0 +1,20 @@
+package indra.curso.mvc;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import indra.curso.mvc.beans.Login;
+
+
+
+public class LoginTest {
+	
+	private Login login;
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
